c7d65b2172e1bc6a0d556edf30fd8e9063c56ee7
Karthik Ramachandran Murugesan <rmkarthik@hotmail.com>
2019-11-17 20:20:54 -0500

    Basic security

--- FlightSearch/src/main/java/com/expediagroup/webapp/Controller/FlightController.java
+++ FlightSearch/src/main/java/com/expediagroup/webapp/Controller/FlightController.java
@@ -3,6 +3,7 @@ package com.expediagroup.webapp.Controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,12 +22,15 @@ import com.expediagroup.webapp.service.FlightService;
  * 
  */
 @RestController
+@Scope("session")
 public class FlightController {
 	
 	@Autowired
 	private FlightService flightService;
 	
 	private Logger logger = LoggerFactory.getLogger(FlightController.class);
+	
+	private int count;
 	
 	/*
 	 * Creates and send the flight to service object.
@@ -45,6 +49,12 @@ public class FlightController {
 	@GetMapping(path = "/flight/{flightTime}", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Object> searchFlights(@PathVariable String flightTime) {
 		return flightService.findFlights(flightTime);
+	}
+	
+	@GetMapping(path = "/count")
+	public String setCount() {
+		count += 1;
+		return "Count is "+count+" after req";
 	}
 }
 
